645ac4da67f8ef060b7f507e5be544efc5dcc80f
IlusCode <iluscode@localhost.localdomain>
2018-05-19 13:21:57 -0500

    modificado:     src/generador/de/pdf/GeneradorDePdf.java

--- src/generador/de/pdf/GeneradorDePdf.java
+++ src/generador/de/pdf/GeneradorDePdf.java
@@ -5,19 +5,9 @@
  */
 package generador.de.pdf;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.pdf.BaseFont;
-import com.itextpdf.text.pdf.PdfContentByte;
-import com.itextpdf.text.pdf.PdfReader;
-import com.itextpdf.text.pdf.PdfStamper;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
 import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
